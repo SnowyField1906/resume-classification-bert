4911f748a97409733cfe0adea65656e34f38eee5
vgkdev <vogiakhang1507@gmail.com>
2024-06-20 23:11:15 +0700

    config cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 
 from main import load, train
+from flask_cors import CORS 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/process', methods=['GET'])
 def process():
